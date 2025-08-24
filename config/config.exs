8380660121deac2da18639c3c313179f8c42104e
Sundar Nambuvel <sunprema@gmail.com>
2025-08-23 19:57:15 -0500

    Auth gen changes

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :sync_me, :scopes,
+  user: [
+    default: true,
+    module: SyncMe.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: SyncMe.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :sync_me,
   ecto_repos: [SyncMe.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
