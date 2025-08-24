75fba4ac40952e8239d7a91d57e1e908472c49ae
Sundar Nambuvel <sunprema@gmail.com>
2025-08-23 19:47:59 -0500

    ueberauth changes added

--- config/config.exs
+++ config/config.exs
@@ -63,3 +63,14 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+
+#Google Auth config
+config :ueberauth, Ueberauth,
+  providers: [
+    google: {Ueberauth.Strategy.Google, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_CLIENT_ID"),
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule SyncMe.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ueberauth_google, "~> 0.10"} #This is for Google auth
     ]
   end
 
