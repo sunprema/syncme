8b85ea6f088a8b6efb616ee8cf4ee978ccd580cc
Sundar Nambuvel <sunprema@gmail.com>
2025-11-10 19:35:54 -0600

    dir changes

--- lib/sync_me/blockchain/contracts/syncme_escrow.ex
+++ lib/sync_me/blockchain/contracts/syncme_escrow.ex
@@ -1,6 +1,6 @@
 defmodule SyncMe.Blockchain.Contracts.SyncMeEscrow do
   use Ethers.Contract,
-    abi_file: "smart_contracts/syncme.abi.json"
+    abi_file: "priv/smart_contracts/syncme.abi.json"
 
   def contract_address() do
     Application.get_env(:sync_me, :blockchain) |> Keyword.get(:syncme_escrow_contract_address)
