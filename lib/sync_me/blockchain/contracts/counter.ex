90c2d3d72db17f9a457429372080640a09e6ab5c
Sundar Nambuvel <sunprema@gmail.com>
2025-11-10 19:22:26 -0600

    removed unnecessary files

--- lib/sync_me/blockchain/contracts/counter.ex
+++
@@ -1,5 +0,0 @@
-defmodule SyncMe.Blockchain.Contracts.Counter do
-  use Ethers.Contract,
-    abi_file: "smart_contracts/counter.abi.json",
-    default_address: Application.compile_env(:sync_me, [:blockchain, :counter_contract_address])
-end
